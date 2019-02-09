157d750f93b971f1d005d7fa8e38ff6d932df70c
DanQuixot <mrborisov1@gmail.com>
2019-02-09 20:01:16 +0000

    added randon movie rendering

--- src/components/Film.js
+++ src/components/Film.js
@@ -14,44 +14,71 @@ class Film extends Component {
       poster: '',
       metascore: '',
       imdbRating: '',
-      rottenTomatoesRating: '',
+      rottenTomatoesRating: 'N/A',
       imdbID: '',
       boxOffice: '',
       plot: ''
     };
   };
-  componentDidMount() {
-
-    fetch('http://www.omdbapi.com/?i=tt0114369&apikey=1681159f')
+  componentWillMount() {
+    // Generate random page number
+    const random = Math.floor(Math.random() * 500);
+    //TMDB initial fetch
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e1c16f19acb1ba81e7eff26f92ed5a48&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${random}&release_date.gte=1996-01-01`)
       .then(response => {
         return response.json();
-      }).then(data => {
-        this.setState({
-          title: data.Title,
-          year: data.Year,
-          rated: data.Rated,
-          runtime: data.Runtime,
-          genre: data.Genre,
-          director: data.Director,
-          actors: data.Actors,
-          poster: data.Poster,
-          metascore: data.Metascore,
-          imdbRating: data.imdbRating,
-          rottenTomatoesRating: data.Ratings[1].Value,
-          imdbID: data.imdbID,
-          boxOffice: data.BoxOffice,
-          plot: data.Plot
-        })
       })
-
+      .then(initialTMDBData => {
+        // Pickout TMDB movie id - random out of 20 results on the page
+        const tmdbID = initialTMDBData.results[Math.floor(Math.random() * 19)].id;
+        // fetch TMDB movie info by tmdbID
+        fetch(`https://api.themoviedb.org/3/movie/${tmdbID}?api_key=e1c16f19acb1ba81e7eff26f92ed5a48`)
+          .then(response => {
+            return response.json();
+          })
+          .then(movieTMDBData => {
+            //Find and set imdbID
+            this.setState({ imdbID: movieTMDBData.imdb_id })
+            // fetch OMDB movie info by imdbID
+            fetch(`http://www.omdbapi.com/?i=${this.state.imdbID}&apikey=1681159f`)
+              .then(response => {
+                return response.json();
+              }) // Set state from OMDB API data
+              .then(data => {
+                let rottenTomatoesRating = 'N/A';
+                let ratingsObject = data.Ratings ? data.Ratings.find(object => {
+                  return object.Source === "Rotten Tomatoes";
+                }) : undefined;
+                if (ratingsObject) {
+                  rottenTomatoesRating = ratingsObject.Value;
+                }
+                this.setState({
+                  title: data.Title,
+                  year: data.Year,
+                  rated: data.Rated,
+                  runtime: data.Runtime,
+                  genre: data.Genre,
+                  director: data.Director,
+                  actors: data.Actors,
+                  poster: data.Poster,
+                  metascore: data.Metascore,
+                  rottenTomatoesRating: rottenTomatoesRating,
+                  imdbRating: data.imdbRating,
+                  boxOffice: data.BoxOffice,
+                  plot: data.Plot
+                })
+              })
+          })
+      })
   };
+
   render() {
     return (
-      <div className="container shadow-lg mt-2 p-5 bg-light border border-light rounded">
+      <div className="container shadow-lg mt-2 p-4 bg-light border border-light rounded" >
         <div className="container">
           <div className="row">
             <div className="col-lg-6">
-              <img src={this.state.poster} alt="poster" className="mx-auto d-block" id="poster" />
+              <img src={this.state.poster} alt="poster" className="mx-auto d-block my-3" id="poster" />
             </div>
             <div className="col-lg-6">
               <h2 className="text-center my-3"><b>{this.state.title} ({this.state.year})</b></h2>
@@ -90,10 +117,10 @@ class Film extends Component {
             </div>
           </div>
         </div>
-        <div className="row mt-4 ">
+        <div className="row mt-2 ">
           <div className="col-12 d-flex justify-content-center">
-            <button className="btn btn-danger m-1"><i className="fas fa-arrow-left mt-1"></i> It's a miss</button>
-            <button className="btn btn-success px-3 m-1">Like this <i className="fas fa-arrow-right mt-1"></i></button>
+            <button className="btn btn-danger px-5 py-1 m-2"><i className="my-thumbs far fa-thumbs-down"></i></button>
+            <button className="btn btn-success px-5 py-1 m-2"><i className="my-thumbs far fa-thumbs-up"></i></button>
           </div>
         </div>
       </div>
