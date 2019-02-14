c2ef643161f9efb1f3af53dbe92ae0a4e8ff3d04
DanQuixot <mrborisov1@gmail.com>
2019-02-14 20:52:33 +0000

    Updates

---
+++ precache-manifest.11ddecd2584f91dbf996eb08b1224cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0e73ce212e3906f7e8e",
+    "url": "/movie-picker/static/css/main.daf93c5e.chunk.css"
+  },
+  {
+    "revision": "f0e73ce212e3906f7e8e",
+    "url": "/movie-picker/static/js/main.f0e73ce2.chunk.js"
+  },
+  {
+    "revision": "63d1ba6359be5b5a29c4",
+    "url": "/movie-picker/static/js/1.63d1ba63.chunk.js"
+  },
+  {
+    "revision": "c236a5a8e5b2d6c4195d",
+    "url": "/movie-picker/static/js/runtime~main.c236a5a8.js"
+  },
+  {
+    "revision": "ea90bbd43cae95353fba51b1c267cafc",
+    "url": "/movie-picker/static/media/background.ea90bbd4.jpg"
+  },
+  {
+    "revision": "176fc0086e5b1c105744084e14799a81",
+    "url": "/movie-picker/index.html"
+  }
+];
